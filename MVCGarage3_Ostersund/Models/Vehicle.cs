9527037b9f81e21e716e2913c8f37e18cd8b80f4
scErikL <erik.lovbom@yahoo.se>
2017-05-29 22:17:45 +0200

    Vehicle model

---
+++ MVCGarage3_Ostersund/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGarage3_Ostersund.Models
+{
+    public enum VehicleType
+    {
+        Car,
+        Snowmobile,
+        Bus,
+        Motorcycle,
+        Lorry
+    };
+    
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string RegNr { get; set; }
+        public VehicleType type { get; set; }
+    }
+}
