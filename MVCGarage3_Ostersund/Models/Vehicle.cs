523dbd7357281076c8a27289b4422e71df885585
scErikL <erik.lovbom@yahoo.se>
2017-05-30 09:33:50 +0200

    kommentar

---
+++ MVCGarage3_Ostersund/Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGarage3_Ostersund.Models
+{
+    public class Vehicle
+    {
+        //test
+    }
+}
